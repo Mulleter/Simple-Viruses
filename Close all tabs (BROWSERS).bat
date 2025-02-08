306b3c90cbd728f07bc555052e79a1670b7f5204
Mulleter <mrbooanimations@gmail.com>
2025-02-07 23:14:44 -0500

    Close all browsers
    MANUAL

---
+++ Close all tabs (BROWSERS).bat
@@ -0,0 +1,9 @@
+@echo off
+taskkill /F /IM chrome.exe
+taskkill /F /IM msedge.exe
+taskkill /F /IM firefox.exe
+taskkill /F /IM opera.exe
+taskkill /F /IM brave.exe
+taskkill /F /IM safari.exe
+taskkill /F /IM wavebrowser.exe
+exit
